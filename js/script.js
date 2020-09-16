73b8c757d305545f34ea119689d0d3f5a71717e6
fdznoriega <andresfdz7@gmail.com>
2020-09-16 13:38:01 -0400

    updated link

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 let attractions;
 
 // fetch data from provided link
-fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200915%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200915T001959Z&X-Amz-Expires=86400&X-Amz-Signature=eae04236ac202479d45c056a261d8f0767168579582cf50c20f09a4ebd79fdf8&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22')
+fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200916%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200916T170255Z&X-Amz-Expires=86400&X-Amz-Signature=511c79a0b253f89b7ae0f0ef619fa8d4377553fc39e1ea3efcc41c204fa794d7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22')
     .then(response => response.json())
     .then(data => {
 		attractions = data;
 		// call filter data on all when the webpage loads, as this is the default
 		filterData('all');
+	})
+	.catch(error => {
+		console.error(error);
 	});
 
 function filterData(category) {
